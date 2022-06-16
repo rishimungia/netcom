97ccba2e521fe8eb19120a977ad6e5ad182a3d8f
Rishi Mungia <rishi.mungia@gmail.com>
2022-06-16 09:35:47 +0530

    Add files via upload

---
+++ index.c
@@ -0,0 +1,9 @@
+Ex2 - Linux Commands
+Ex3 - Error Detection and Control mechanisms [Parity Check + Checksum + CRC]
+Ex4 - Flow control mechanisms [Stop and Wait ARQ + Go back N ARQ + Selective Repeat ARQ]
+Ex5 - IP addressing and Classless addressing
+Ex6 - Wireshark
+Ex7 - Socket programming for TCP, UDP and multi-client chatting [Server + Client]
+Ex8 - Simulation of Unicast Routing Protocols [Link state routing + Distance Vector Routing Algorithm using Bellman Ford's Algorithm]
+Ex9 - Analysis of Congestion Control Techniques in network [Leaky Bucket algorithm]
+Ex10 - Develop a DNS client server to resolve IP address or Host name
